ed973c3da72db064c0ef7fc4bc90553a520012df
xxxhakuaxxx <naoki.06110201@gmail.com>
2022-03-01 12:44:21 +0900

    chapter3途中(3/1)

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -7,38 +7,8 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response) {
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello/Index</title>
-    <style>
-    body {
-        font-size: 16px;
-        color: #999;
-    }
-
-    h1 {
-        font-size: 120px;
-        text-align: right;
-        color: #afafa;
-        margin: -50px 0px -120px 0px;
-    }
-    </style>
-    </head>
-
-    <body>
-        <h1>Hello</h1>
-        <h3>Request</h3>
-        <pre>{$request}</pre>
-        <h3>Response</h3>
-        <pre>{$response}</pre>
-    </body>
-
-    </html>
-    EOF;
-
-        $response->setContent($html);
-        return $response;
+    public function index() {
+        $data = ['msg'=>'これはコントローラから渡されたメッセージです。'];
+        return view('hello.index', $data);
     }
 }
---
+++ laravelapp/resources/views/hello/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+    <title>Hello/Index</title>
+    <style>
+    body {
+        font-size: 16px;
+        color: #999;
+    }
+
+    h1 {
+        font-size: 120px;
+        text-align: right;
+        color: #f6f6f6;
+        margin: -50px 0px -120px 0px;
+    }
+    </style>
+    </head>
+
+    <body>
+        <h1>Index</h1>
+        <p><?php echo $msg; ?></p>
+        <p><?php echo date("Y年n月j日"); ?>
+    </body>
+
+    </html>
