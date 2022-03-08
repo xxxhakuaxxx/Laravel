923b810deb1c48b6154e6a0da65882e7234c9550
xxxhakuaxxx <naoki.06110201@gmail.com>
2022-03-08 12:35:50 +0900

    104ページのみ

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -11,22 +11,6 @@
     <p>ここが本文のコンテンツです。</p>
     <p>Controller value<br>'message' = {{$message}}</p>
     <p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
-    <!-- <ul>
-        @each('components.item', $data, 'item')
-    </ul> -->
-    <!-- <p>必要なだけ記述できます。</p> -->
-
-    <!-- @component('components.message')
-        @slot('msg_title')
-        CAUTION!
-        @endslot
-
-        @slot('msg_content')
-        これはメッセージの表示です。
-        @endslot
-    @endcomponent -->
-
-    <!-- @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビューです。']) -->
 @endsection
 
 @section('footer')
