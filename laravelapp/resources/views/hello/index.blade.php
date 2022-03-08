6ba1c4bbc973d6ee6624234a601605563e5a818d
xxxhakuaxxx <naoki.06110201@gmail.com>
2022-03-08 10:40:21 +0900

    while(3/8 p84まで)

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -1,36 +1,39 @@
 <html>
-    <head>
+
+<head>
     <title>Hello/Index</title>
     <style>
-    body {
-        font-size: 16px;
-        color: #999;
-    }
+        body {
+            font-size: 16px;
+            color: #999;
+        }
 
-    h1 {
-        font-size: 50px;
-        text-align: right;
-        color: #f6f6f6;
-        margin: -20px 0px -30px 0px;
-        letter-spacing: -4px;
-    }
+        h1 {
+            font-size: 50px;
+            text-align: right;
+            color: #f6f6f6;
+            margin: -20px 0px -30px 0px;
+            letter-spacing: -4px;
+        }
     </style>
-    </head>
+</head>
 
-    <body>
-        <h1>Blade/Index</h1>
-        <p>&#064;forディレクティブの例</p>
-        @foreach ($data as $item)
-        @if ($loop->first)
-        <p>※データ一覧</p>
-        <ul>
-            @endif
-            <li>No,{{$loop->iteration}}. {{$item}}</li>
-            @if ($loop->last)
-        </ul>
-        <p>—―ここまで</p>
-        @endif
-        @endforeach
-    </body>
+<body>
+    <h1>Blade/Index</h1>
+    <p>&#064;whileディレクティブの例</p>
+    <ol>
+        @php
+            $counter = 0;
+        @endphp
+        @while ($counter < count($data)) 
+            <li>
+                {{$data[$counter]}}
+            </li>
+            @php
+                $counter++
+            @endphp
+        @endwhile
+    </ol>
+</body>
 
-    </html>
+</html>
