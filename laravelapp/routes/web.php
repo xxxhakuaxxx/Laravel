9a146cf95247f09f1c4a13d9410aeef21892eedb
xxxhakuaxxx <naoki.06110201@gmail.com>
2022-03-09 10:25:05 +0900

    3/9実習問題

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -26,8 +26,10 @@ Route::post('hello', 'HelloController@post');
 
 Route::get('jissyu2', 'JissyuController@index');        /* Chapter2_実習問題_1 */
 
-Route::get('jissyu3', 'jissyu3_1Controller@index');     /* Chapter3_1実習問題_1_2 */
+Route::get('jissyu3', 'jissyu3_1Controller@index');     /* Chapter3_1実習問題_1 */
 Route::post('jissyu3', 'jissyu3_1Controller@post');
 
-Route::get('jissyu4', 'jissyu3_2Controller@index');     /* Chapter3_2実習問題_1_2 */
+Route::get('jissyu4', 'jissyu3_2Controller@index');     /* Chapter3_2実習問題_2 */
 Route::post('jissyu4', 'jissyu3_2Controller@post');
+
+Route::get('jissyu5', 'jissyu3_3Controller@index');     /* Chapter3_３実習問題_３ */
