153780baf8025086aab4c6613778d7012798ac08
xxxhakuaxxx <naoki.06110201@gmail.com>
2022-03-10 12:16:27 +0900

    p148まで(3/10)

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\Validator; 
 
 class HelloController extends Controller
 {
@@ -13,12 +15,28 @@ class HelloController extends Controller
     }
 
     public function post(Request $request) {
-        $validate_rule = [
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/hello')
+                            ->withErrors($validator)
+                            ->withInput();
+        }
         return view('hello.index', ['msg'=>'正しく入力されました！']);
+
+        /* $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $msg = 'クエリーに問題があります。';
+        } else {
+            $msg = 'iD/PASSを受け付けました。フォーム入力ください。';
+        }
+        return view('hello.index', ['msg'->$msg, ]); */
     }
 }
